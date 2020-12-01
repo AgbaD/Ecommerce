959942200b0767ed55f0fe7444ecd4b19b6f1373
Damilare <blankgodd33@gmail.com>
2020-12-01 12:49:08 +0100

    memo api 100

--- memo_api/app/main/mainapp.py
+++ memo_api/app/main/mainapp.py
@@ -35,62 +35,69 @@ def token_required(f):
 @main.route("/api/get_user/<public_id>", methods=["GET"])
 @token_required
 def get_user(current_user, public_id):
-    if not current_user.admin:
-        return jsonify({"status": "error", "msg": "Can not perform action"}), 403
+    try:
+        if not current_user.admin:
+            return jsonify({"status": "error", "msg": "Can not perform action"}), 403
 
-    user = User.query.filter_by(public_id=public_id).first()
-    if not user:
-        return jsonify({"status": "error", "msg": "User not found"}), 404
-    data = {'email': user.email, 'name': user.name,
-            'admin': user.admin, "public_id": user.public_id,
-            "memos": {}}
-    memos = Memo.query.all()
-    if memos:
-        for memo in memos:
-            if memo.user_id == user.id:
+        user = User.query.filter_by(public_id=public_id).first()
+        if not user:
+            return jsonify({"status": "error", "msg": "User not found"}), 404
+        data = {'email': user.email, 'name': user.name,
+                'admin': user.admin, "public_id": user.public_id,
+                "memos": {}}
+        memos = Memo.query.filter_by(user_id=user.id).all()
+        if memos:
+            for memo in memos:
                 data['memos'][memo.id] = memo.title
 
-    return jsonify(data)
+        return jsonify(data)
+    except:
+        return jsonify({"status": "error", "msg": "Internal Server Error"}), 500
 
 
 @main.route("/api/get_me", methods=["GET"])
 @token_required
 def get_me(current_user):
-    data = {'email': current_user.email, 'name': current_user.name,
-            'admin': current_user.admin, "public_id": current_user.public_id,
-            "memos": {}}
+    try:
+        data = {'email': current_user.email, 'name': current_user.name,
+                'admin': current_user.admin, "public_id": current_user.public_id,
+                "memos": {}}
 
-    memos = Memo.query.all()
-    if memos:
-        for memo in memos:
-            if memo.user_id == current_user.id:
+        memos = Memo.query.filter_by(user_id=current_user.id).all()
+        if memos:
+            for memo in memos:
                 data['memos'][memo.id] = memo.title
 
-    return jsonify(data)
+        return jsonify(data)
+    except:
+        return jsonify({"status": "error", "msg": "Internal Server Error"}), 500
 
 
 @main.route("/api/get_all_users", methods=["GET"])
 @token_required
 def get_all_users(current_user):
-    if not current_user.admin:
-        return jsonify({"status": "error", "msg": "Can not perform action"}), 403
+    try:
+        if not current_user.admin:
+            return jsonify({"status": "error", "msg": "Can not perform action"}), 403
 
-    all_users = User.query.all()
-    memos = Memo.query.all()
+        all_users = User.query.all()
+        memos = Memo.query.all()
 
-    output = []
-    for user in all_users:
-        data = {'email': user.email, 'public_id': user.public_id,
-                'name': user.name, 'active': user.active,
-                'memos': {}}
-        if memos:
-            for mem in memos:
-                if mem.user_id == user.id:
-                    data['memos'][mem.id] = mem.title
+        output = []
+        for user in all_users:
+            data = {'email': user.email, 'public_id': user.public_id,
+                    'name': user.name, 'active': user.active,
+                    'memos': {}}
+            if memos:
+                for mem in memos:
+                    if mem.user_id == user.id:
+                        data['memos'][mem.id] = mem.title
 
-        output.append(data)
+            output.append(data)
 
-    return jsonify({'users': output})
+        return jsonify({'users': output})
+    except:
+        return jsonify({"status": "error", "msg": "Internal Server Error"}), 500
 
 
 @main.route("/api/create_acc", methods=["POST"])
@@ -127,21 +134,24 @@ def create_acc():
 @main.route("/api/edit_me", methods=["PUT"])
 @token_required
 def edit_me(current_user):
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    if "name" in data:
-        current_user.name = data['name']
-    if "email" in data:
-        current_user.email = data['email']
-    if "password" in data:
-        current_user.password = data['password']
-    if "admin" in data:
-        if data['admin'] == "True":
-            current_user.admin = True
+        if "name" in data:
+            current_user.name = data['name']
+        if "email" in data:
+            current_user.email = data['email']
+        if "password" in data:
+            current_user.password = data['password']
+        if "admin" in data:
+            if data['admin'] == "True":
+                current_user.admin = True
 
-    db.session.commit()
+        db.session.commit()
 
-    return jsonify({'status': "success", "msg": "User as successfully updated"})
+        return jsonify({'status': "success", "msg": "User as successfully updated"})
+    except:
+        return jsonify({"status": "error", "msg": "Internal Server Error"}), 500
 
 
 @main.route("/api/login", methods=["POST"])
@@ -174,45 +184,89 @@ def login():
 @main.route("/api/get_memo/<memo_id>", methods=['GET'])
 @token_required
 def get_memo(current_user, memo_id):
-    memo = Memo.query.filter_by(id=memo_id).first()
-    if memo.user_id != current_user.id:
-        if not current_user.admin:
-            return jsonify({"status": "error", "msg": "Can not perform action"}), 403
-    if not memo:
-        return jsonify({'status': "error", "msg": "Memo not found"})
+    try:
+        memo = Memo.query.filter_by(id=memo_id).first()
+        if not memo:
+            return jsonify({'status': "error", "msg": "Memo not found"})
+        if memo.user_id != current_user.id:
+            if not current_user.admin:
+                return jsonify({"status": "error", "msg": "Can not perform action"}), 403
 
-    data = {"id": memo.id, "date": memo.date, "title": memo.title, "text": memo.text}
+        data = {"id": memo.id, "date": memo.date, "title": memo.title, "text": memo.text}
 
-    return jsonify(data)
+        return jsonify(data)
+    except:
+        return jsonify({"status": "error", "msg": "Internal Server Error"}), 500
 
 
 @main.route("/api/get_memos", methods=['GET'])
 @token_required
 def get_memos(current_user):
-    memos = Memo.query.all()
+    try:
+        memos = Memo.query.all()
 
-    data = {}
-    if memos:
-        for memo in memos:
-            if memo.user_id == current_user.id:
-                data[memo.id] = {"date": memo.date, "title": memo.title, "text": memo.text}
+        data = {}
+        if memos:
+            for memo in memos:
+                if memo.user_id == current_user.id:
+                    data[memo.id] = {"date": memo.date, "title": memo.title, "text": memo.text}
 
-    return jsonify(data)
+        return jsonify(data)
+    except:
+        return jsonify({"status": "error", "msg": "Internal Server Error"}), 500
 
 
 @main.route("/api/create_memo", methods=['POST'])
 @token_required
-def create_memos(current_user):
-    pass
+def create_memo(current_user):
+    try:
+        data = request.get_json()
+
+        title = data['title']
+        text = data['text']
+        memo = Memo(title=title, text=text, date=datetime.datetime.utcnow(), user_id=current_user.id)
+
+        db.session.add(memo)
+        db.session.commit()
+        return jsonify({'status': "success", "msg": "Memo successfully created"}), 201
+    except:
+        return jsonify({"status": "error", "msg": "Internal Server Error"}), 500
 
 
 @main.route("/api/edit_memo/<memo_id>", methods=['PUT'])
 @token_required
-def edit_memos(current_user, memo_id):
-    pass
+def edit_memo(current_user, memo_id):
+    try:
+        data = request.get_json()
+        memo = Memo.query.filter_by(id=memo_id).first()
+        if memo.user_id != current_user.id:
+            return jsonify({"status": "error", "msg": "Can not perform action"}), 403
+
+        if "title" in data:
+            memo.title = data['title']
+        if 'text' in data:
+            memo.text = data['text']
+        if "title" in data or "text" in data:
+            memo.date = datetime.datetime.utcnow()
+
+        db.session.commit()
+
+        return jsonify({"status": "success", "msg": "Memo updated"})
+    except:
+        return jsonify({"status": "error", "msg": "Internal Server Error"}), 500
 
 
 @main.route("/api/delete_memo/<memo_id>", methods=['DELETE'])
 @token_required
-def delete_memos(current_user, memo_id):
-    pass
+def delete_memo(current_user, memo_id):
+    try:
+        memo = Memo.query.filter_by(id=memo_id).first()
+        if memo.user_id != current_user.id:
+            return jsonify({"status": "error", "msg": "Can not perform action"}), 403
+
+        db.session.delete(memo)
+        db.session.commit()
+
+        return jsonify({"status": "success", "msg": "Memo deleted successfully"})
+    except:
+        return jsonify({"status": "error", "msg": "Internal Server Error"}), 500
