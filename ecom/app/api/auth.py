0c1fe2446635cf21153efb096c94eb9626e397c4
Damilare Agba <damilareagba1@gmail.com>
2021-07-20 17:45:25 +0100

    jwt authentication

--- ecom/app/api/auth.py
+++ ecom/app/api/auth.py
@@ -6,51 +6,87 @@ from flask import jsonify, request, current_app
 import jwt
 
 import uuid
+from functools import wraps
 from datetime import datetime, timedelta
 
 from . import api
 from .. import mysql
 
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({
+                'status': 'error', 'message': 'Token is missing!'
+            }), 401
+
+        try:
+            data = jwt.decode(token, current_app.config['SECRET_KEY'])
+            cur = mysql.connection.cursor()
+            cur.execute(
+                'SELECT * FROM users WHERE public_id={}'.format(data['public_id'])
+            )
+
+            current_user = cur.fetchone()
+        except Exception as e:
+            return jsonify({
+                'status': 'error', 'message': 'Token is invalid!'
+            }), 401
+
+        return f(current_user, *args, **kwargs)
+    return decorated
+
+
 @api.route('/api/v1/login', methods=['POST'])
 def login():
     if request.method == "POST":
-        data = request.get_json()
+        try:
+            data = request.get_json()
 
-        password = data['password']
-        email = data['email']
+            password = data['password']
+            email = data['email']
 
-        cur = mysql.connection.cursor()
-        cur.execute(
-            "SELECT * FROM users WHERE email={}".format(email)
-        )
+            cur = mysql.connection.cursor()
+            cur.execute(
+                "SELECT * FROM users WHERE email={}".format(email)
+            )
 
-        user = cur.fetchone()
-        if not user:
+            user = cur.fetchone()
+            if not user:
+                return jsonify({
+                    'status': 'error', 'message': 'User not found'
+                }), 401
+            cur.close()
+
+            if not check_password_hash(user.password_hash, password):
+                return jsonify({
+                    'status': 'error', 'message': 'Password is incorrect'
+                }), 401
+
+            token = jwt.encode({
+                'public_id': user.public_id, 'exp': datetime.utcnow() + timedelta(minutes=90)
+            }, current_app.config['SECRET_KEY'])
+
             return jsonify({
-                'status': 'error', 'status_code': 400, 'message': 'User not found'
-            })
-        cur.close()
-
-        if not check_password_hash(user.password_hash, password):
+                'status': 'success', 'message': 'Login successful',
+                'data': {
+                    'token': token.decode('UTF-8')
+                }
+            }), 200
+        except Exception as e:
             return jsonify({
-                'status': 'error', 'status_code': 400, 'message': 'Password is incorrect'
-            })
-
-        token = jwt.encode({
-            'public_id': user.public_id, 'exp': datetime.utcnow() + timedelta(minutes=90)
-        }, current_app.config['SECRET_KEY'])
-
-        return jsonify({
-            'status': 'success', 'status_code': 200, 'message': 'Login successful',
-            'data': {
-                'token': token.decode('UTF-8')
-            }
-        })
+                'status': 'error', 'message': e
+            }), 500
     else:
         return jsonify({
-            'status': 'error', 'status_code': 400, 'message': "Endpoint doesn't support GET requests"
-        })
+            'status': 'error', 'message': "Endpoint doesn't support GET requests"
+        }), 400
 
 
 @api.route('/api/v1/register', methods=['POST'])
@@ -69,8 +105,8 @@ def register():
 
             if password != re_password:
                 return jsonify({
-                    'status': 'error', 'status_code': 400, 'message': 'Passwords do not match'
-                })
+                    'status': 'error', 'message': 'Passwords do not match'
+                }), 400
 
             cur = mysql.connection.cursor()
             user = None
@@ -85,8 +121,8 @@ def register():
             cur.close()
             if user:
                 return jsonify({
-                    'status': 'error', 'status_code': 400, 'message': 'Email has ben used!'
-                })
+                    'status': 'error', 'message': 'Email has ben used!'
+                }), 400
 
             password_hash = generate_password_hash(password)
             public_id = str(uuid.uuid4())
@@ -99,15 +135,15 @@ def register():
             mysql.connection.commit()
 
             return jsonify({
-                'status': 'success', 'status_code': 200, 'message': 'User created successfully'
-            })
+                'status': 'success', 'message': 'User created successfully'
+            }), 200
         except Exception as e:
             return jsonify({
-                'status': 'error', 'status_code': 500, 'message': e
-            })
+                'status': 'error', 'message': e
+            }), 500
 
     else:
         return jsonify({
-            'status': 'error', 'status_code': 400, 'message': "Endpoint doesn't support GET requests"
-        })
+            'status': 'error', 'message': "Endpoint doesn't support GET requests"
+        }), 400
 
---
+++ ecom/app/api/utils/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author:   @AgbaD || @agba_dr3
+
+
+
+
