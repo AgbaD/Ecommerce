3debb68889eedd9bfa3944f05d5abd64158aba5a
Damilare Agba <damilareagba1@gmail.com>
2021-08-11 03:24:26 +0100

    creating products

--- ecom/app/backend/merchant.py
+++ ecom/app/backend/merchant.py
@@ -270,6 +270,10 @@ def create_product(data):
     }
 
 
+def get_product(product_pid):
+    pass
+
+
 def update_product(product_pid, data):
     product = Product.query.filter_by(public_id=product_pid).first()
     if not product:
